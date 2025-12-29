063ed57ec0bfb4eeaec40a031e0f700ae468b564
Himanshu Dobhal <himanshu.dobhal@gmail.com>
2025-12-28 19:24:08 -0500

    Setup Completed for IDE

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from pathlib import Path
+
+import pandas as pd
+
+
 def main():
-    print("Hello from my-ml-project!")
+    data_path = Path("data") / "supercars.csv"
+    print(f"--- Loading data from {data_path} ---")
+    df = pd.read_csv(data_path)
+    print("--- Dataset Head ---")
+    print(df.head())
 
 
 if __name__ == "__main__":
